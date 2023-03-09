90e7b8a92762638cd1c2d48f6f3e10703ab6c29e
focodes <sdjimenez861@soy.sena.edu.co>
2023-03-09 14:32:06 -0500

    objectos

---
+++ objetos/objetos.js
@@ -0,0 +1,12 @@
+// Entidades representadas a traves de propiedades 
+
+const libro = {
+  color: 'Verde',
+  titulo: 'EL poder del ahora',
+  paginas: 323,
+  autor: 'Eckhart Tolle'
+};
+
+// Se puede acceder a las propiedades de un objeto mediante la notacion de corchetes
+
+console.log(libro.titulo)
