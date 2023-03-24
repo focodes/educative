d5672b91d4e12cb406bde13443ffd6b514f24126
develocod <sdjimenez861@soy.sena.edu.co>
2023-03-24 06:49:22 -0500

    no habia hecho este commit

---
+++ DOM/element.js
@@ -0,0 +1,5 @@
+console.log(document.body.childNodes[5].childNodes[1].innerHTML);
+
+const titleElements = document.getElementsByTagName("h2");
+console.log(titleElements[0].innerHTML);
+console.log((titleElements.length))
