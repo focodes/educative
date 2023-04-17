01dbbf38b4bfb27658e1716bd8447e90c8a0a1b1
focodes <sdjimenez861@soy.sena.edu.co>
2023-04-17 16:53:02 -0500

    Agregando animaciones, ejercicios y condicionales

---
+++ condicionales/operadorLogico.js
@@ -0,0 +1,7 @@
+//comprobar si el numero se encuentra en un rango 
+let numeroAleatorio;
+numeroAleatorio = Number(prompt('Elija el numero a evaluar: '));
+
+if ((numeroAleatorio >= 0) && (numeroAleatorio <= 100)){
+  console.log(`Su número es ${numeroAleatorio} y está entre 0 y 100 incluidos`);
+}
